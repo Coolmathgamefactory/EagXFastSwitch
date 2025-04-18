7731d66b6f08a4c5ae131421139d7f05b6f338c6
MuddyFrog24 <156368825+Coolmathgamefactory@users.noreply.github.com>
2025-04-18 14:08:29 -0400

    EagXFastSwitch

--- jetpack-ef (1).js
+++ jetpack-ef (1).js
@@ -1,19 +1,16 @@
 ModAPI.require("player"); //We need to add vertical velocity
-var jetpackActive = false; //Variable to track wether or not the jetpack is active
+var gamemode = 1
 window.addEventListener("keydown", (event) => { //When a key is pressed
-  if (event.key.toLowerCase() === "h") { //If the key is h
-    jetpackActive = true; //Set jetpack to be active
+  if (event.key.toLowerCase() === "k") { //If the key is k
+    ModAPI.player.sendChatMessage({message:"/gamemode "+gamemode})
+    gamemode = gamemode -1;
   }
 });
-window.addEventListener("keyup", (event) => { //When a key is released
-  if (event.key.toLowerCase() === "h") { //If the key is h
-    jetpackActive = false; //Set jetpack to be inactive
+window.addEventListener("keydown", (event) => { //When a key is pressed
+  if (event.key.toLowerCase() === "l") { //If the key is l
+    ModAPI.player.sendChatMessage({message:"/gamemode "+gamemode})
+    gamemode = gamemode +1;
   }
 });
-ModAPI.addEventListener("update", ()=>{ //Every client tick
-  if(!jetpackActive){ //If the jetpack is not active
-    return; //Exit
-  }
-  ModAPI.player.motionY += 0.2; //Add 0.2 to the players vertical moition
   ModAPI.player.reload(); //Push changes
 });
